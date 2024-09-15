3d4a8868d2c6cb99c3ee9018fb7d15fdafc51e8c
Jonah Bedouch <jonah@bedouch.net>
2024-09-14 22:07:10 -0700

    Implement code for autograder zip

---
+++ autograder-zip/setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cd /autograder/source
+
+apt-get install -y python python-pip python-dev
+
+mkdir -p /root/.ssh
+cp ssh_config /root/.ssh/config
+# Make sure to include your private key here
+cp deploy_key /root/.ssh/deploy_key
+# To prevent host key verification errors at runtime
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+
+# Clone autograder files
+git clone git@github.com:hkn-mu/decal-attend.git /autograder/autograder_samples
+
+# Checkout the right repository
+cd /autograder/autograder_samples
+git checkout fa24
+
+# Install python dependencies
+pip install -r /autograder/autograder_samples/requirements.txt
